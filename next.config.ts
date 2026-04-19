e315ebfe2d3259e8142a7707491d99988302159f
Charles Campos <charles.campos@ramarim.com.br>
2026-04-19 16:28:04 -0300

    Forçando exportação estática

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "export",
   eslint: {
-    // Permite que o Netlify publique o app mesmo se houver avisos de código
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // Permite que o Netlify publique o app mesmo se houver avisos de tipagem
     ignoreBuildErrors: true,
   },
 };
