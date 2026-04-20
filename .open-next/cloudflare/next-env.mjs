e7fbdb91b060eab51f61d5f91b36c85772682d59
Charles Campos <seu-email-pessoal@gmail.com>
2026-04-20 11:08:33 -0300

    fix: corrige package.json + cf build

---
+++ .open-next/cloudflare/next-env.mjs
@@ -0,0 +1,3 @@
+export const production = {"NEXT_PUBLIC_SUPABASE_URL":"https://saowxftmogykiaqmhrfs.supabase.co","NEXT_PUBLIC_SUPABASE_ANON_KEY":"sb_publishable_ayasgbKjK15XxtPqiQRpZg_e3WzFLq3"};
+export const development = {"NEXT_PUBLIC_SUPABASE_URL":"https://saowxftmogykiaqmhrfs.supabase.co","NEXT_PUBLIC_SUPABASE_ANON_KEY":"sb_publishable_ayasgbKjK15XxtPqiQRpZg_e3WzFLq3"};
+export const test = {};
---
+++ .open-next/server-functions/default/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"bujeeSNaI61yDvAI8lrPeam4CJB1UUfkPyXt0NOjPjk=\"\n}"
