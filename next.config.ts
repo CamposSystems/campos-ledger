06305490ebce1b6dda8ce4813e5200e993759218
Charles Campos <seu-email-pessoal@gmail.com>
2026-04-22 14:56:42 -0300

    Removendo plugin da Cloudflare do Next config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   reactStrictMode: true,
 };
 
 export default nextConfig;
-import('@opennextjs/cloudflare').then(m => m.initOpenNextCloudflareForDev());
